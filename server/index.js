81386866676a34f07ff5f81bf53082dd2d8af5a8
Rihem BEN SAAD <rihambensaad@gmail.com>
2023-02-23 21:26:07 +0100

    send Hello Clediss

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+app.get("/", (req, res) => {
+    res.send("Hello Clediss")
+})
+
 app.listen(5000, () => {
     console.log("server is running on port 5000")
 })
