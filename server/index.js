6fbba67542151685d4a23adcf85a8395a97a1974
Rihem BEN SAAD <rihambensaad@gmail.com>
2023-02-24 20:25:11 +0100

    insert

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mysql = require('mysql');
 const cors = require("cors");
+
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "crud_contact"
+})
 
 app.use(cors());
 app.use(express.json());
@@ -9,8 +18,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", (req, res) => {
-    res.send("Hello Clediss")
-})
+    const sql =
+     "INSERT INTO contact_db (name, email, contact) VALUES ('rihem', 'rihem@gmail.com', 298159)";
+    db.query(sql, (error, result) => {
+        console.log("error", error);
+        console.log("result", result);
+        res.send("Hello Clediss");
+    });
+});
 
 app.listen(5000, () => {
     console.log("server is running on port 5000")
